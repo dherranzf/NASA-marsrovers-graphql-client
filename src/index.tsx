4e9410be605e351d9c9cbd5a1ad47bb4cabac1ce
dherranzf <dhfdev14@gmail.com>
2025-05-10 17:16:35 +0200

    Add modal component for informational messages and integrate into app

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { ThemeProvider } from './context/ThemeContext';
+import Modal from "./components/modal";
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_MARS_GRAPHQL_SERVER_URL, 
@@ -11,12 +12,32 @@ const client = new ApolloClient({
 
 const root = createRoot(document.getElementById('root')!);
 
-root.render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <ApolloProvider client={client}>
-        <Pages />
-      </ApolloProvider>
-    </ThemeProvider>
-  </React.StrictMode>
-);
+const App = () => {
+  const [showModal, setShowModal] = useState(true);
+
+  return (
+    <React.StrictMode>
+      <ThemeProvider>
+        <ApolloProvider client={client}>
+          {showModal && (
+            <Modal
+              icon="info"
+              message={
+                <>
+                  <p>This application is a proof of concept hosted on a free server.</p>
+                  <p>If the server has been inactive for a while, it may take a few minutes to start.</p>
+                  <p>Please wait patiently for a few minutes to start seeing Mars photos.</p>
+                  <p>Thank you for your patience!</p>
+                </>
+              }
+              onClose={() => setShowModal(false)}
+            />
+          )}
+          <Pages />
+        </ApolloProvider>
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+};
+
+root.render(<App />);
